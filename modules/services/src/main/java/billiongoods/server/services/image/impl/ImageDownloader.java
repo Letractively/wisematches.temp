9a4ff85d6b568c62cf0c3538bdfc0b09efb742b5
smklimenko <smklimenko@localhost>
2013-08-02 22:23:45 +0400

    +

--- modules/services/src/main/java/billiongoods/server/services/image/impl/ImageDownloader.java
+++ modules/services/src/main/java/billiongoods/server/services/image/impl/ImageDownloader.java
@@ -9,7 +9,10 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Sergey Klimenko (smklimenko@gmail.com)
@@ -21,30 +24,46 @@ public class ImageDownloader {
     public static void main(String[] args) throws IOException {
         final List<String> strings = FileUtils.readLines(new File(args[0]));
 
-        int index = 0;
         int number = 0;
+        int startPoint = 0;
+
         Path outPath = null;
-        String currentSku = null;
+
+        final Map<String, AtomicInteger> skus = new HashMap<>();
+
 
         final Path outFolder = new File(args[1]).toPath();
         for (String line : strings) {
-            final String[] split = line.split(",");
-
-            final String sku = split[0];
-            final String url = split[1];
-
-            if (!sku.equalsIgnoreCase(currentSku)) { // new sku
-                index = 0;
-                outPath = outFolder.resolve(sku);
-                Files.createDirectories(outPath);
-                currentSku = sku;
+            if (number < startPoint) {
+                number++;
+                continue;
             }
 
-            final Path resolve = outPath.resolve(String.valueOf(index++) + ".jpg");
+            try {
+                final String[] split = line.split(",");
 
-            try (InputStream inputStream = new URL(url).openStream()) {
-                System.out.println("[" + (number++) + " of " + strings.size() + " ] Copy image from " + url + " to " + resolve);
-                Files.copy(inputStream, resolve, StandardCopyOption.REPLACE_EXISTING);
+                final String sku = split[0].trim();
+                final String url = split[1].trim();
+
+                AtomicInteger atomicInteger = skus.get(sku);
+                if (atomicInteger == null) {
+                    outPath = outFolder.resolve(sku);
+                    Files.createDirectories(outPath);
+
+                    atomicInteger = new AtomicInteger();
+                    skus.put(sku, atomicInteger);
+                }
+
+                final Path resolve = outPath.resolve(String.valueOf(atomicInteger.incrementAndGet()) + ".jpg");
+
+                try (InputStream inputStream = new URL(url).openStream()) {
+                    System.out.println("ERROR: [" + number + " of " + strings.size() + " ] Copy image from " + url + " to " + resolve);
+                    Files.copy(inputStream, resolve, StandardCopyOption.REPLACE_EXISTING);
+                }
+            } catch (Exception ex) {
+                System.out.println("Data can't be loaded from: " + line + ". " + ex.getMessage());
+            } finally {
+                number++;
             }
         }
     }
