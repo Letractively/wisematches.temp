ce34762b93722b5763668a333bc59ece2f00c65f
smklimenko <smklimenko@localhost>
2013-08-02 08:40:49 +0400

    +

--- modules/services/src/main/java/billiongoods/server/services/image/impl/ImageDownloader.java
+++ modules/services/src/main/java/billiongoods/server/services/image/impl/ImageDownloader.java
@@ -1,10 +1,15 @@
 package billiongoods.server.services.image.impl;
 
-import java.io.*;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 /**
  * @author Sergey Klimenko (smklimenko@gmail.com)
@@ -14,15 +19,15 @@ public class ImageDownloader {
     }
 
     public static void main(String[] args) throws IOException {
-        final Path outFolder = new File(args[1]).toPath();
-        final BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        final List<String> strings = FileUtils.readLines(new File(args[0]));
 
         int index = 0;
+        int number = 0;
         Path outPath = null;
         String currentSku = null;
 
-        String line = reader.readLine();
-        while (line != null) {
+        final Path outFolder = new File(args[1]).toPath();
+        for (String line : strings) {
             final String[] split = line.split(",");
 
             final String sku = split[0];
@@ -38,11 +43,9 @@ public class ImageDownloader {
             final Path resolve = outPath.resolve(String.valueOf(index++) + ".jpg");
 
             try (InputStream inputStream = new URL(url).openStream()) {
-                System.out.println("Copy image from " + url + " to " + resolve);
+                System.out.println("[" + (number++) + " of " + strings.size() + " ] Copy image from " + url + " to " + resolve);
                 Files.copy(inputStream, resolve, StandardCopyOption.REPLACE_EXISTING);
             }
-
-            line = reader.readLine();
         }
     }
 }
