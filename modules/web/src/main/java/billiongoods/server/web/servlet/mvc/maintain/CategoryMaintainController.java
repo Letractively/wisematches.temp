16b3ecd6d6f41a26afe8da9ef72ff41d1154ebea
smklimenko <smklimenko@localhost>
2013-08-06 08:51:41 +0400

    +

--- modules/web/src/main/java/billiongoods/server/web/servlet/mvc/maintain/CategoryMaintainController.java
+++ modules/web/src/main/java/billiongoods/server/web/servlet/mvc/maintain/CategoryMaintainController.java
@@ -11,12 +11,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -25,76 +28,91 @@ import java.util.Set;
 @Controller
 @RequestMapping("/maintain/category")
 public class CategoryMaintainController extends AbstractController {
-	private CategoryManager categoryManager;
-	private AttributeManager attributeManager;
+    private CategoryManager categoryManager;
+    private AttributeManager attributeManager;
 
-	public CategoryMaintainController() {
-	}
+    public CategoryMaintainController() {
+    }
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
-	public String viewCategory(Model model, @ModelAttribute("form") CategoryForm form) {
-		Category category = null;
-		if (form.getId() != null) {
-			category = categoryManager.getCategory(form.getId());
-		}
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String viewCategory(Model model, @ModelAttribute("form") CategoryForm form) {
+        Category category = null;
+        if (form.getId() != null) {
+            category = categoryManager.getCategory(form.getId());
+        }
 
-		if (category != null) {
-			form.setName(category.getName());
-			form.setDescription(category.getDescription());
-			if (category.getParent() != null) {
-				form.setParent(category.getParent().getId());
-			} else {
-				form.setParent(null);
-			}
+        if (category != null) {
+            form.setName(category.getName());
+            form.setDescription(category.getDescription());
+            if (category.getParent() != null) {
+                form.setParent(category.getParent().getId());
+            } else {
+                form.setParent(null);
+            }
 
-			int index = 0;
-			final Set<Attribute> attributes = category.getAttributes();
-			final Integer[] attrs = new Integer[attributes.size()];
-			for (Attribute attribute : attributes) {
-				attrs[index++] = attribute.getId();
-			}
-			form.setAttributes(attrs);
-		}
+            final Set<Attribute> attributes = category.getAttributes();
+            final List<Integer> attrs = new ArrayList<>();
+            for (Attribute attribute : attributes) {
+                attrs.add(attribute.getId());
+            }
 
-		model.addAttribute("category", category);
+            Category parent = category.getParent();
+            while (parent != null) {
+                for (Attribute attribute : parent.getAttributes()) {
+                    attrs.remove(attribute.getId());
+                }
+                parent = category.getParent();
+            }
 
-		model.addAttribute("catalog", categoryManager.getCatalog());
-		model.addAttribute("attributes", attributeManager.getAttributes());
-		return "/content/maintain/category";
-	}
+            form.setAttributes(attrs);
+            model.addAttribute("category", category);
+        }
 
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	@RequestMapping(value = "", method = RequestMethod.POST)
-	public String updateCategory(Model model, @Valid @ModelAttribute("form") CategoryForm form) {
-		final Set<Attribute> attrs = new HashSet<>();
-		final Integer[] attributes = form.getAttributes();
-		if (attributes != null) {
-			for (Integer attribute : attributes) {
-				attrs.add(attributeManager.getAttribute(attribute));
-			}
-		}
+        model.addAttribute("catalog", categoryManager.getCatalog());
+        model.addAttribute("attributes", attributeManager.getAttributes());
+        return "/content/maintain/category";
+    }
 
-		Category parent = null;
-		if (form.getParent() != null) {
-			parent = categoryManager.getCategory(form.getParent());
-		}
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String updateCategory(Model model, @Valid @ModelAttribute("form") CategoryForm form, Errors errors) {
+        try {
+            final Set<Attribute> attrs = new HashSet<>();
+            final List<Integer> attributes = form.getAttributes();
+            if (attributes != null) {
+                for (Integer attribute : attributes) {
+                    attrs.add(attributeManager.getAttribute(attribute));
+                }
+            }
 
-		final Category category;
-		if (form.getId() == null) {
-			category = categoryManager.createCategory(form.getName(), form.getDescription(), attrs, parent, form.getPosition());
-		} else {
-			category = categoryManager.updateCategory(form.getId(), form.getName(), form.getDescription(), attrs, parent, form.getPosition());
-		}
-		return "redirect:/maintain/category?id=" + category.getId();
-	}
+            Category parent = null;
+            if (form.getParent() != null) {
+                parent = categoryManager.getCategory(form.getParent());
+            }
 
-	@Autowired
-	public void setCategoryManager(CategoryManager categoryManager) {
-		this.categoryManager = categoryManager;
-	}
+            final Category category;
+            if (form.getId() == null) {
+                category = categoryManager.createCategory(form.getName(), form.getDescription(), attrs, parent, form.getPosition());
+            } else {
+                category = categoryManager.updateCategory(form.getId(), form.getName(), form.getDescription(), attrs, parent, form.getPosition());
+            }
+            return "redirect:/maintain/category?id=" + category.getId();
+        } catch (Exception ex) {
+            errors.reject("", ex.getMessage());
+        }
 
-	@Autowired
-	public void setAttributeManager(AttributeManager attributeManager) {
-		this.attributeManager = attributeManager;
-	}
+        model.addAttribute("catalog", categoryManager.getCatalog());
+        model.addAttribute("attributes", attributeManager.getAttributes());
+        return "/content/maintain/category";
+    }
+
+    @Autowired
+    public void setCategoryManager(CategoryManager categoryManager) {
+        this.categoryManager = categoryManager;
+    }
+
+    @Autowired
+    public void setAttributeManager(AttributeManager attributeManager) {
+        this.attributeManager = attributeManager;
+    }
 }
