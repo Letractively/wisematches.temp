096ea93d95f8d5d9ed6308ce1024e25d568db289
smklimenko <smklimenko@localhost>
2013-08-07 16:29:53 +0400

    +

--- modules/services/src/main/java/billiongoods/server/services/image/ImageSize.java
+++ modules/services/src/main/java/billiongoods/server/services/image/ImageSize.java
@@ -1,8 +1,13 @@
 package billiongoods.server.services.image;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,18 +17,20 @@ import java.io.OutputStream;
  * Enumeration that contains availeable sizes of player images.
  */
 public enum ImageSize {
-	TINY(50, 50),
-	SMALL(150, 150),
-	MEDIUM(280, 280),
-	LARGE(600, 600);
+	TINY(50, 50, .9f),
+	SMALL(150, 150, .85f),
+	MEDIUM(280, 280, .85f),
+	LARGE(600, 600, .85f);
 
 	private final int width;
 	private final int height;
 	private final String code;
+	private final float compression;
 
-	ImageSize(int width, int height) {
+	ImageSize(int width, int height, float compression) {
 		this.width = width;
 		this.height = height;
+		this.compression = compression;
 		this.code = name().substring(0, 1);
 	}
 
@@ -40,9 +47,32 @@ public enum ImageSize {
 	}
 
 	public void scaleImage(InputStream in, OutputStream out) throws IOException {
-		final BufferedImage read = ImageIO.read(in);
+		final ImageReader imgReader = ImageIO.getImageReadersByFormatName("jpeg").next();
+		final ImageWriter imgWriter = ImageIO.getImageWritersByFormatName("jpg").next();
+
+		final ImageInputStream imgIn = ImageIO.createImageInputStream(in);
+		final ImageOutputStream imgOut = ImageIO.createImageOutputStream(out);
+
+		imgReader.setInput(imgIn);
+		imgWriter.setOutput(imgOut);
+
+		final BufferedImage read = imgReader.read(0);
 		final BufferedImage bufferedImage = scaleImage(read);
-		ImageIO.write(bufferedImage, "JPEG", out);
+
+		final IIOImage image = new IIOImage(bufferedImage, null, imgReader.getImageMetadata(0));
+
+		final JPEGImageWriteParam params = (JPEGImageWriteParam) imgWriter.getDefaultWriteParam();
+		params.setCompressionMode(JPEGImageWriteParam.MODE_EXPLICIT);
+		params.setCompressionQuality(compression);
+		params.setOptimizeHuffmanTables(true);
+
+		imgWriter.write(null, image, params);
+
+		imgIn.close();
+		imgOut.close();
+
+		imgWriter.dispose();
+		imgReader.dispose();
 	}
 
 	private float getScaleFactor(BufferedImage image) {
@@ -64,16 +94,14 @@ public enum ImageSize {
 		final int w = (int) (scale * image.getWidth());
 		final int h = (int) (scale * image.getHeight());
 
+		final Image scaledInstance = image.getScaledInstance(w, h, Image.SCALE_AREA_AVERAGING);
+
 		final BufferedImage scaled = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		final Graphics2D bg = scaled.createGraphics();
 		bg.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-
-		// scale image
-		final AffineTransform at = new AffineTransform();
-		at.scale(scale, scale);
-		at.translate((width - w) / 2, (height - h) / 2);
-		bg.drawRenderedImage(image, at);
+		bg.drawImage(scaledInstance, 0, 0, width, height, null);
 		bg.dispose();
+
 		return scaled;
 	}
 }
