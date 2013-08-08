30d72b5c5b0070930ab34908d7ecbb25864db2aa
smklimenko <smklimenko@localhost>
2013-08-08 10:30:38 +0400

    +

---
+++ modules/services/src/main/java/billiongoods/server/test/notify/impl/publisher/MailNotificationPublisher.java
@@ -0,0 +1,151 @@
+package billiongoods.server.test.notify.impl.publisher;
+
+
+import billiongoods.core.Language;
+import billiongoods.server.services.ServerDescriptor;
+import billiongoods.server.test.notify.Notification;
+import billiongoods.server.test.notify.NotificationScope;
+import billiongoods.server.test.notify.NotificationSender;
+import billiongoods.server.test.notify.PublicationException;
+import billiongoods.server.test.notify.impl.NotificationPublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.InternetAddress;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Sergey Klimenko (smklimenko@gmail.com)
+ */
+public class MailNotificationPublisher implements NotificationPublisher {
+	private JavaMailSender mailSender;
+	private MessageSource messageSource;
+	private ServerDescriptor serverDescriptor;
+
+	private final Map<SenderKey, InternetAddress> addressesCache = new HashMap<>();
+
+	private static final Logger log = LoggerFactory.getLogger("billiongoods.notification.MailPublisher");
+
+	public MailNotificationPublisher() {
+	}
+
+	@Override
+	public String getName() {
+		return "email";
+	}
+
+	@Override
+	public NotificationScope getNotificationScope() {
+		return NotificationScope.EXTERNAL;
+	}
+
+	@Override
+	public void publishNotification(final Notification notification) throws PublicationException {
+		throw new UnsupportedOperationException("TODO: commented");
+
+/*
+		log.debug("Send mail notification '{}' to {}", notification.getCode(), notification.getTarget());
+        final MimeMessagePreparator mm = new MimeMessagePreparator() {
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                final Member member = notification.getTarget();
+                final Language language = member.getLanguage();
+
+                final InternetAddress to = new InternetAddress(member.getEmail(), member.getUsername(), "UTF-8");
+                final InternetAddress from = getInternetAddress(notification.getSender(), language);
+
+                final MimeMessageHelper msg = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+                msg.setFrom(from);
+                msg.setTo(to);
+                msg.setSubject(notification.getSubject());
+
+                final StringBuilder m = new StringBuilder();
+                final Locale locale = notification.getTarget().getLanguage().getLocale();
+                m.append(messageSource.getMessage("notify.mail.header", null, locale));
+                m.append(" <b>").append(notification.getTarget().getUsername()).append("</b>.");
+                m.append(notification.getMessage());
+                m.append("<p><hr><br>");
+                m.append(messageSource.getMessage("notify.mail.footer", null, locale));
+                m.append("</p>");
+                msg.setText(m.toString(), true);
+            }
+        };
+        try {
+            mailSender.send(mm);
+        } catch (MailException ex) {
+            throw new PublicationException(ex);
+        }
+*/
+	}
+
+	protected InternetAddress getInternetAddress(NotificationSender sender, Language language) {
+		return addressesCache.get(new SenderKey(sender, language));
+	}
+
+	private void validateAddressesCache() {
+		addressesCache.clear();
+
+		if (messageSource == null || serverDescriptor == null) {
+			return;
+		}
+
+		for (NotificationSender sender : NotificationSender.values()) {
+			for (Language language : Language.values()) {
+				try {
+					final String address = messageSource.getMessage("mail.address." + sender.getUserInfo(),
+							null, sender.getMailAddress(serverDescriptor), language.getLocale());
+
+					final String personal = messageSource.getMessage("mail.personal." + sender.getUserInfo(),
+							null, sender.name(), language.getLocale());
+
+					addressesCache.put(new SenderKey(sender, language), new InternetAddress(address, personal, "UTF-8"));
+				} catch (UnsupportedEncodingException ex) {
+					log.error("JAVA SYSTEM ERROR - NOT UTF8!", ex);
+				}
+			}
+		}
+	}
+
+	public void setMailSender(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+		validateAddressesCache();
+	}
+
+	public void setServerDescriptor(ServerDescriptor serverDescriptor) {
+		this.serverDescriptor = serverDescriptor;
+		validateAddressesCache();
+	}
+
+	private static final class SenderKey {
+		private final Language language;
+		private final NotificationSender sender;
+
+		private SenderKey(NotificationSender sender, Language language) {
+			this.sender = sender;
+			this.language = language;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+
+			SenderKey senderKey = (SenderKey) o;
+			return language == senderKey.language && sender == senderKey.sender;
+		}
+
+		@Override
+		public int hashCode() {
+			int result = language.hashCode();
+			result = 31 * result + sender.hashCode();
+			return result;
+		}
+	}
+}
---
+++ modules/services/src/test/java/billiongoods/server/test/state/impl/SessionRegistryStateManagerTest.java
@@ -0,0 +1,58 @@
+package billiongoods.server.test.state.impl;
+
+import org.junit.Test;
+
+/**
+ * @author Sergey Klimenko (smklimenko@gmail.com)
+ */
+public class SessionRegistryStateManagerTest {
+	public SessionRegistryStateManagerTest() {
+	}
+
+	@Test
+	public void test() {
+/*
+		final Player player1 = new DefaultVisitor(Language.RU);
+        final Player player2 = new DefaultVisitor(Language.EN);
+
+        final PlayerStateListener listener = createStrictMock(PlayerStateListener.class);
+
+        final SessionRegistryStateManager stateManager = new SessionRegistryStateManager();
+        stateManager.addPlayerStateListener(listener);
+
+        listener.playerOnline(player1);
+        listener.playerOnline(player2);
+        listener.playerAlive(player1);
+        listener.playerAlive(player1);
+        listener.playerOffline(player2);
+        listener.playerOffline(player1);
+        replay(listener);
+
+        assertFalse(stateManager.isPlayerOnline(player1));
+        stateManager.registerNewSession("S1", new MemberDetails(player1, "asd", "qwe", false, false, Arrays.asList("mock")));
+        assertTrue(stateManager.isPlayerOnline(player1));
+        stateManager.registerNewSession("S2", new MemberDetails(player1, "asd", "qwe", false, false, Arrays.asList("mock")));
+        assertFalse(stateManager.isPlayerOnline(player2));
+        stateManager.registerNewSession("S3", new MemberDetails(player2, "asd", "qwe", false, false, Arrays.asList("mock")));
+        assertTrue(stateManager.isPlayerOnline(player2));
+        stateManager.registerNewSession("S4", new MemberDetails(player2, "asd", "qwe", false, false, Arrays.asList("mock")));
+        stateManager.refreshLastRequest("S5");
+        stateManager.refreshLastRequest("S1");
+        stateManager.refreshLastRequest("S1");
+        stateManager.removeSessionInformation("S5");
+        assertTrue(stateManager.isPlayerOnline(player1));
+        assertTrue(stateManager.isPlayerOnline(player2));
+        stateManager.removeSessionInformation("S2");
+        assertTrue(stateManager.isPlayerOnline(player1));
+        stateManager.removeSessionInformation("S3");
+        assertTrue(stateManager.isPlayerOnline(player2));
+        stateManager.removeSessionInformation("S4");
+        assertFalse(stateManager.isPlayerOnline(player2));
+        stateManager.removeSessionInformation("S1");
+        assertFalse(stateManager.isPlayerOnline(player1));
+
+        verify(listener);
+*/
+		throw new UnsupportedOperationException("commented");
+	}
+}
