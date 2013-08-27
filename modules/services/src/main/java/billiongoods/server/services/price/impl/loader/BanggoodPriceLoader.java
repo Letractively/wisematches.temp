32bd6a4be667db711a1438b97e8f6532a31c27d1
smklimenko <smklimenko@localhost>
2013-08-27 22:27:04 +0400

    +

--- modules/services/src/main/java/billiongoods/server/services/price/impl/loader/BanggoodPriceLoader.java
+++ modules/services/src/main/java/billiongoods/server/services/price/impl/loader/BanggoodPriceLoader.java
@@ -4,6 +4,8 @@ import billiongoods.server.services.price.impl.PriceLoader;
 import billiongoods.server.services.price.impl.PriceLoadingException;
 import billiongoods.server.warehouse.Price;
 import billiongoods.server.warehouse.SupplierInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,6 +22,8 @@ import java.util.regex.Pattern;
 public class BanggoodPriceLoader implements PriceLoader {
     private static final Pattern PRICE_PATTERN = Pattern.compile("<div (?:[^\\s]+).*?id=\"(price_sub|regular_div)\".*?>\\(?(.+?)\\)?</div>");
 
+    private static final Logger log = LoggerFactory.getLogger("billiongoods.price.BanggoodPriceLoader");
+
     public BanggoodPriceLoader() {
     }
 
@@ -31,6 +35,16 @@ public class BanggoodPriceLoader implements PriceLoader {
             urlConnection.setUseCaches(false);
             urlConnection.setDefaultUseCaches(false);
             urlConnection.setInstanceFollowRedirects(true);
+
+            urlConnection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            urlConnection.setRequestProperty("Accept-Charset", "windows-1251,utf-8;q=0.7,*;q=0.3");
+            urlConnection.setRequestProperty("Accept-Language", "ru,en;q=0.8");
+            urlConnection.setRequestProperty("Cache-Control", "max-age=0");
+            urlConnection.setRequestProperty("Connection", "keep-alive");
+            urlConnection.setRequestProperty("Host", "www.banggood.com");
+            urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 YaBrowser/1.7.1364.21027 Safari/537.22");
+            urlConnection.setReadTimeout(5000);
+            urlConnection.setConnectTimeout(5000);
 
             try (final InputStream inputStream = urlConnection.getInputStream()) {
                 final Price price = parsePrice(inputStream);
@@ -47,10 +61,14 @@ public class BanggoodPriceLoader implements PriceLoader {
     private Price parsePrice(InputStream in) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
+        final StringBuilder sb = new StringBuilder();
+
         double price = Double.NaN;
         int linesAfterPrice = -1;
         String s = reader.readLine();
         while (s != null) {
+            sb.append(s.trim());
+
             final Matcher matcher = PRICE_PATTERN.matcher(s.trim());
             if (matcher.matches()) {
                 final String type = matcher.group(1);
@@ -73,6 +91,7 @@ public class BanggoodPriceLoader implements PriceLoader {
 
             s = reader.readLine();
         }
+        log.info("Server response: {}", sb);
         return null;
     }
 }
