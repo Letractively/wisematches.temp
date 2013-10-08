fb0b02f66676e3f8f69b65bfc3d9c193d325c03e
smklimenko <smklimenko@localhost>
2013-10-08 22:00:05 +0400

    +

--- modules/web/src/main/java/billiongoods/server/web/servlet/mvc/warehouse/form/SortingType.java
+++ modules/web/src/main/java/billiongoods/server/web/servlet/mvc/warehouse/form/SortingType.java
@@ -6,35 +6,35 @@ import billiongoods.core.search.Order;
  * @author Sergey Klimenko (smklimenko@gmail.com)
  */
 public enum SortingType {
-	BESTSELLING("bs", "stockInfo.sold", false),
-	PRICE_DOWN("plth", "price", true),
-	PRICE_UP("phtl", "price", false),
-	ARRIVAL_DATE("d", "registrationDate", false);
+    BESTSELLING("bs", "soldCount", false),
+    PRICE_DOWN("plth", "price", true),
+    PRICE_UP("phtl", "price", false),
+    ARRIVAL_DATE("d", "registrationDate", false);
 
-	private final String code;
-	private final Order order;
+    private final String code;
+    private final Order order;
 
-	private static final SortingType[] values = SortingType.values();
+    private static final SortingType[] values = SortingType.values();
 
-	SortingType(String code, String property, boolean ask) {
-		this.code = code;
-		this.order = ask ? Order.asc(property) : Order.desc(property);
-	}
+    SortingType(String code, String property, boolean ask) {
+        this.code = code;
+        this.order = ask ? Order.asc(property) : Order.desc(property);
+    }
 
-	public String getCode() {
-		return code;
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public Order getOrder() {
-		return order;
-	}
+    public Order getOrder() {
+        return order;
+    }
 
-	public static SortingType byCode(String code) {
-		for (SortingType sortingType : values) {
-			if (sortingType.code.equalsIgnoreCase(code)) {
-				return sortingType;
-			}
-		}
-		return null;
-	}
+    public static SortingType byCode(String code) {
+        for (SortingType sortingType : values) {
+            if (sortingType.code.equalsIgnoreCase(code)) {
+                return sortingType;
+            }
+        }
+        return null;
+    }
 }
