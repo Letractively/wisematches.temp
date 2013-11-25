f841d09ff9fa6d0c22b478322b7418df2651b77f
smklimenko <smklimenko@localhost>
2013-11-25 19:44:51 +0400

    +

--- resources/database/latest_patch.sql
+++ resources/database/latest_patch.sql
@@ -3,6 +3,20 @@ ALTER TABLE `billiongoods`.`account_personality`
 CHANGE COLUMN `password` `password` VARCHAR(100) NULL,
 CHANGE COLUMN `email` `email` VARCHAR(150) NULL;
 
+CREATE TABLE account_UserConnection (userId         VARCHAR(255) NOT NULL,
+                                     providerId     VARCHAR(255) NOT NULL,
+                                     providerUserId VARCHAR(255),
+                                     rank           INT          NOT NULL,
+                                     displayName    VARCHAR(255),
+                                     profileUrl     VARCHAR(512),
+                                     imageUrl       VARCHAR(512),
+                                     accessToken    VARCHAR(255) NOT NULL,
+                                     secret         VARCHAR(255),
+                                     refreshToken   VARCHAR(255),
+                                     expireTime     BIGINT,
+  PRIMARY KEY (userId, providerId, providerUserId));
+CREATE UNIQUE INDEX UserConnectionRank ON account_UserConnection (userId, providerId, rank);
+
 INSERT INTO `billiongoods`.`system_version` (`version`) VALUES ('1125');
 
 #11.12
