095e004c01fc94aeb5a9c147bc4327cf6d17bc91
smklimenko <smklimenko@localhost>
2014-02-18 16:29:02 +0400

    +

--- resources/database/latest_patch.sql
+++ resources/database/latest_patch.sql
@@ -1,3 +1,18 @@
+#14.02.14
+ALTER TABLE `billiongoods`.`store_order`
+ADD COLUMN `phone` VARCHAR(20) NULL DEFAULT NULL
+AFTER `city`;
+
+ALTER TABLE `billiongoods`.`privacy_address_record`
+CHANGE COLUMN `postcode` `postcode` VARCHAR(10) NULL DEFAULT NULL,
+CHANGE COLUMN `region` `region` VARCHAR(145) NULL DEFAULT NULL,
+CHANGE COLUMN `city` `city` VARCHAR(145) NULL DEFAULT NULL,
+ADD COLUMN `phone` VARCHAR(20) NULL DEFAULT NULL
+AFTER `lastName`;
+
+
+INSERT INTO `billiongoods`.`system_version` (`version`) VALUES ('140214');
+
 #14.02.12
 ALTER TABLE `billiongoods`.`store_category`
 ADD COLUMN `symbolic` VARCHAR(155) NULL DEFAULT NULL
