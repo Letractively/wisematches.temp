3f9e07442dd2eefde66b5f60a4204a01e56acd93
sergey <sergey@hpnotebook>
2014-06-08 21:33:55 +0400

    +

--- modules/web/src/main/java/billiongoods/server/web/servlet/mvc/maintain/BangGoodImportController.java
+++ modules/web/src/main/java/billiongoods/server/web/servlet/mvc/maintain/BangGoodImportController.java
@@ -39,144 +39,144 @@ import java.util.zip.ZipFile;
 @Controller
 @RequestMapping("/maintain/product")
 public class BangGoodImportController extends AbstractController {
-	private ProductManager productManager;
-	private ProductImporter productImporter;
-	private SupplierDataLoader supplierDataLoader;
+    private ProductManager productManager;
+    private ProductImporter productImporter;
+    private SupplierDataLoader supplierDataLoader;
 
-	public BangGoodImportController() {
-	}
+    public BangGoodImportController() {
+    }
 
-	@RequestMapping(value = "/import", method = RequestMethod.GET)
-	public String importProductsView(@RequestParam(value = "c", required = false) Integer categoryId,
-									 @ModelAttribute("form") ImportProductsForm form, Model model) {
+    @RequestMapping(value = "/import", method = RequestMethod.GET)
+    public String importProductsView(@RequestParam(value = "c", required = false) Integer categoryId,
+                                     @ModelAttribute("form") ImportProductsForm form, Model model) {
 
-		final ImportingSummary summary = productImporter.getImportingSummary();
-		if (summary != null) {
-			model.addAttribute("summary", summary);
-		} else {
-			if (categoryId != null) {
-				final Category category = categoryManager.getCategory(categoryId);
-				form.setCategory(categoryId);
+        final ImportingSummary summary = productImporter.getImportingSummary();
+        if (summary != null) {
+            model.addAttribute("summary", summary);
+        } else {
+            if (categoryId != null) {
+                final Category category = categoryManager.getCategory(categoryId);
+                form.setCategory(categoryId);
 
-				model.addAttribute("category", category);
-				model.addAttribute("attributes", ProductMaintainController.createAttributesMap(category).keySet());
-			}
-		}
-		return "/content/maintain/import";
-	}
+                model.addAttribute("category", category);
+                model.addAttribute("attributes", ProductMaintainController.createAttributesMap(category).keySet());
+            }
+        }
+        return "/content/maintain/import";
+    }
 
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	@RequestMapping(value = "/import", method = RequestMethod.POST)
-	public String importProductsAction(Model model, @ModelAttribute("form") ImportProductsForm form) {
-		try {
-			List<Property> properties = null;
-			final Integer[] propertyIds = form.getPropertyIds();
-			final String[] propertyValues = form.getPropertyValues();
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @RequestMapping(value = "/import", method = RequestMethod.POST)
+    public String importProductsAction(Model model, @ModelAttribute("form") ImportProductsForm form) {
+        try {
+            List<Property> properties = null;
+            final Integer[] propertyIds = form.getPropertyIds();
+            final String[] propertyValues = form.getPropertyValues();
 
-			if (propertyIds != null && propertyValues != null && propertyIds.length > 0) {
-				properties = new ArrayList<>();
-				for (int i = 0; i < propertyIds.length; i++) {
-					try {
-						final Integer p = propertyIds[i];
-						final String v = propertyValues[i];
-						if (!v.isEmpty()) {
-							final Attribute attribute = attributeManager.getAttribute(p);
-							properties.add(new Property(attribute, v));
-						}
-					} catch (Exception ignore) {
-					}
-				}
-			}
+            if (propertyIds != null && propertyValues != null && propertyIds.length > 0) {
+                properties = new ArrayList<>();
+                for (int i = 0; i < propertyIds.length; i++) {
+                    try {
+                        final Integer p = propertyIds[i];
+                        final String v = propertyValues[i];
+                        if (!v.isEmpty()) {
+                            final Attribute attribute = attributeManager.getAttribute(p);
+                            properties.add(new Property(attribute, v));
+                        }
+                    } catch (Exception ignore) {
+                    }
+                }
+            }
 
-			List<Integer> groups = null;
-			if (form.getParticipatedGroups() != null) {
-				groups = new ArrayList<>();
-				for (Integer integer : form.getParticipatedGroups()) {
-					if (integer != null) {
-						groups.add(integer);
-					}
-				}
-			}
+            List<Integer> groups = null;
+            if (form.getParticipatedGroups() != null) {
+                groups = new ArrayList<>();
+                for (Integer integer : form.getParticipatedGroups()) {
+                    if (integer != null) {
+                        groups.add(integer);
+                    }
+                }
+            }
 
-			final Category category = categoryManager.getCategory(form.getCategory());
+            final Category category = categoryManager.getCategory(form.getCategory());
 
 
-			final String uploadUrl = form.getUploadUrl();
-			if (uploadUrl != null) {
-				final Path tempFile = Files.createTempFile("ProductsImporting", "bg");
-				Files.copy(new URL(uploadUrl).openStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
+            final String uploadUrl = form.getUploadUrl();
+            if (uploadUrl != null && !uploadUrl.isEmpty()) {
+                final Path tempFile = Files.createTempFile("ProductsImporting", "bg");
+                Files.copy(new URL(uploadUrl).openStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
 
-				InputStream imagesIS = null;
-				InputStream descriptionIS = null;
+                InputStream imagesIS = null;
+                InputStream descriptionIS = null;
 
-				final ZipFile zipFile = new ZipFile(tempFile.toFile());
-				final Enumeration<? extends ZipEntry> entries = zipFile.entries();
-				while (entries.hasMoreElements()) {
-					final ZipEntry zipEntry = entries.nextElement();
-					final String name = zipEntry.getName();
-					if (name.endsWith("image.csv")) {
-						imagesIS = readZipEntry(zipFile, zipEntry);
-					} else if (name.endsWith("info.csv")) {
-						descriptionIS = readZipEntry(zipFile, zipEntry);
-					}
-				}
-				zipFile.close();
-				Files.deleteIfExists(tempFile);
+                final ZipFile zipFile = new ZipFile(tempFile.toFile());
+                final Enumeration<? extends ZipEntry> entries = zipFile.entries();
+                while (entries.hasMoreElements()) {
+                    final ZipEntry zipEntry = entries.nextElement();
+                    final String name = zipEntry.getName();
+                    if (name.endsWith("image.csv")) {
+                        imagesIS = readZipEntry(zipFile, zipEntry);
+                    } else if (name.endsWith("info.csv")) {
+                        descriptionIS = readZipEntry(zipFile, zipEntry);
+                    }
+                }
+                zipFile.close();
+                Files.deleteIfExists(tempFile);
 
-				if (descriptionIS != null && imagesIS != null) {
-					productImporter.importProducts(category, properties, groups, descriptionIS, imagesIS, form.isValidatePrice());
-				}
-			} else {
-				productImporter.importProducts(category, properties, groups, form.getDescription().getInputStream(),
-						form.getImages().getInputStream(), form.isValidatePrice());
-			}
-			model.addAttribute("result", true);
-			model.addAttribute("category", category);
-			model.addAttribute("attributes", ProductMaintainController.createAttributesMap(category).keySet());
-		} catch (Exception ex) {
-			model.addAttribute("error", ex.getMessage());
-			model.addAttribute("result", false);
-		}
-		return "/content/maintain/import";
-	}
+                if (descriptionIS != null && imagesIS != null) {
+                    productImporter.importProducts(category, properties, groups, descriptionIS, imagesIS, form.isValidatePrice());
+                }
+            } else {
+                productImporter.importProducts(category, properties, groups, form.getDescription().getInputStream(),
+                        form.getImages().getInputStream(), form.isValidatePrice());
+            }
+            model.addAttribute("result", true);
+            model.addAttribute("category", category);
+            model.addAttribute("attributes", ProductMaintainController.createAttributesMap(category).keySet());
+        } catch (Exception ex) {
+            model.addAttribute("error", ex.getMessage());
+            model.addAttribute("result", false);
+        }
+        return "/content/maintain/import";
+    }
 
-	private InputStream readZipEntry(ZipFile zipFile, ZipEntry zipEntry) throws IOException {
-		final InputStream in = zipFile.getInputStream(zipEntry);
+    private InputStream readZipEntry(ZipFile zipFile, ZipEntry zipEntry) throws IOException {
+        final InputStream in = zipFile.getInputStream(zipEntry);
 
-		final ByteArrayOutputStream bos = new ByteArrayOutputStream((int) zipEntry.getSize());
-		int next = in.read();
-		while (next > -1) {
-			bos.write(next);
-			next = in.read();
-		}
-		return new ByteArrayInputStream(bos.toByteArray());
-	}
+        final ByteArrayOutputStream bos = new ByteArrayOutputStream((int) zipEntry.getSize());
+        int next = in.read();
+        while (next > -1) {
+            bos.write(next);
+            next = in.read();
+        }
+        return new ByteArrayInputStream(bos.toByteArray());
+    }
 
-	@RequestMapping(value = "/loadSupplierInfo.ajax")
-	public ServiceResponse changeState(@RequestParam("id") Integer pid, Locale locale) {
-		final SupplierInfo supplier = productManager.getSupplierInfo(pid);
-		if (supplier == null) {
-			responseFactory.failure("error.unknown.supplier", locale);
-		}
-		try {
-			return responseFactory.success(supplierDataLoader.loadDescription(supplier));
-		} catch (DataLoadingException ex) {
-			return responseFactory.failure("error.bad.supplier", locale);
-		}
-	}
+    @RequestMapping(value = "/loadSupplierInfo.ajax")
+    public ServiceResponse changeState(@RequestParam("id") Integer pid, Locale locale) {
+        final SupplierInfo supplier = productManager.getSupplierInfo(pid);
+        if (supplier == null) {
+            responseFactory.failure("error.unknown.supplier", locale);
+        }
+        try {
+            return responseFactory.success(supplierDataLoader.loadDescription(supplier));
+        } catch (DataLoadingException ex) {
+            return responseFactory.failure("error.bad.supplier", locale);
+        }
+    }
 
-	@Autowired
-	public void setProductManager(ProductManager productManager) {
-		this.productManager = productManager;
-	}
+    @Autowired
+    public void setProductManager(ProductManager productManager) {
+        this.productManager = productManager;
+    }
 
-	@Autowired
-	public void setProductImporter(ProductImporter productImporter) {
-		this.productImporter = productImporter;
-	}
+    @Autowired
+    public void setProductImporter(ProductImporter productImporter) {
+        this.productImporter = productImporter;
+    }
 
-	@Autowired
-	public void setSupplierDataLoader(SupplierDataLoader supplierDataLoader) {
-		this.supplierDataLoader = supplierDataLoader;
-	}
+    @Autowired
+    public void setSupplierDataLoader(SupplierDataLoader supplierDataLoader) {
+        this.supplierDataLoader = supplierDataLoader;
+    }
 }
